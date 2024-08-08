69cb1b5ad7f651f724e7e3e8389f60af34b31fd9
gayathri-1609 <218a1a0496@risekrishnasaiprakasam.edu.in>
2024-08-08 12:00:30 +0530

    Add files via upload

---
+++ calculator-main/calculater with  python/narmol.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter first number: "))
+
+num2 = int(input("Enter second number: "))
+
+op = input("Enter operation(+,-,*,/,%): ")
+
+if op == '+':
+    print("Result:", num1 + num2)
+
+elif op == '-':
+    print("Result:", num1 - num2)
+
+elif op == '*':
+    print("Result:", num1* num2)
+
+elif op == '/':
+    if(num2=0){print("Result:not divided by 0")}
+else:{
+    print("Result:", num1 / num2)}
+elif op == '%':
+    print("Result:", num1 % num2)
